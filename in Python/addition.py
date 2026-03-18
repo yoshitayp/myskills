17595a6829ce0e4ff986104519b76398c0c2d772
yoshitayp <yoshitapillay804@gmail.com>
2026-03-18 09:16:08 +0530

    Add addition program in Python

---
+++ in Python/addition.py
@@ -0,0 +1,7 @@
+#This is addtion code in Python
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("The sum of", num1, "and", num2, "is", num1 + num2)
+
