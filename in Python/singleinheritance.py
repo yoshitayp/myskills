8f666951679fef87ac42f25b80f09b9a8fec8d5e
yoshitayp <yoshitapillay804@gmail.com>
2026-04-19 15:08:27 +0530

    sinngle inheritance code in py

--- in Python/singleinheritance.py
+++ in Python/singleinheritance.py
@@ -1,10 +1,9 @@
 class Parent:
-def show(self);
-print("This is Parent class")
+    def show(self):
+        print("This is Parent class")
 class Child(Parent):
     def display(self):
         print("This is Chlid class")
         obj=Child()
         obj.show()
         obj.display()
-        
