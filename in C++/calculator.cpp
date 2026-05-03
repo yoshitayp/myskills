992a9fa9420a57a9a63dee87f455268d01c759fa
yoshitayp <yoshitapillay804@gmail.com>
2026-05-03 22:15:37 +0530

    calculator code in c++ for simple calculations

---
+++ in C++/calculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 5;
+    cout << "Sum: " << a + b << endl;
+    cout << "Difference: " << a - b << endl;
+    cout << "Product: " << a * b << endl;
+    cout << "Quotient: " << a / b << endl;
+    return 0;
+}
+
